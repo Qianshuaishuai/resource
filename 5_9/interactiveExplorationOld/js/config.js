3a0cd6c08b833393294847f47d02f3a1cd36fd0c
QianDashuai <491787891@qq.com>
2019-05-05 17:11:04 +0800

    [add]增加logo

---
+++ 5_9/interactiveExplorationOld/js/config.js
@@ -0,0 +1,33 @@
+﻿/**
+ * Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		//'/',	//换行
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		//'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+	config.removeButtons = 'Outdent,Indent,About,PasteFromWord,Scayt,Image,Table,Maximize,HorizontalRule,NumberedList,BulletedList,Blockquote,Anchor,Unlink,Link,Styles';
+
+	config.language = 'zh-cn';
+	config.width = 420;
+	config.height = 100;
+	config.uiColor = '#FFFFFF';
+	config.toolbarCanCollapse = true;
+	config.resize_enabled = false;
+	config.startupFocus = true;
+};
---
+++ 5_9/interactiveExplorationOld/js/vendor-async.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{"1oZe":function(e,o,s){"use strict";o.__esModule=!0,o.default=function(e){return{methods:{focus:function(){this.$refs[e].focus()}}}}}});
